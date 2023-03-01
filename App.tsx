eabf32573210061f471132500c26641c88a294da
tugba-koc <tugba.koc@sisal.com>
2023-03-01 16:39:30 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): JSX.Element {
+  console.log('hello');
+  return (
+    <SafeAreaView>
+      <Text>Hello!</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
